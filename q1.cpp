b96b96d6cc39883051918ebc67ca4d821d5b1720
Jai-10 <jai.vaidya18@gmail.com>
2022-08-26 15:08:33 +0530

    Partially done (creating checkpoint)

--- q1.cpp
+++ q1.cpp
@@ -1,12 +1,53 @@
 // Name: Jai Vaidya
 // Roll: 2010990758
-// Set No: 
-// Q1: 
+// Set No: 03
+// Q1: Rearrange an array with alternate high and low elements
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
+
+    // input length
+    int length;
+    cout << "Enter length: ";
+    cin >> length;
+
+    // create arr
+    int arr[length];
+
+    // input array
+    cout << "Input array: ";
+    for (int i=0; i<length; i++) {
+        cin >> arr[i];
+    }
+
+
+    // reversing with alternate high and low elements
+    for (int i=1; i<=length-1; i++) {
+        if (arr[i]>arr[i-1] && arr[i]>arr[i+1]) {
+            i += 2;
+        }
+        else {
+            if (arr[i] < arr[i-1]) {
+                int temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = arr[i];
+            }
+            else if (arr[i] < arr[i+1]) {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = arr[i];
+            }
+            i += 2;
+        }
+    }
+
+
+    // printing the array
+    for (int i=0; i<length; i++) {
+        cout << arr[i] << " ";
+    } cout << endl;
 
 
 
--- q2.cpp
+++ q2.cpp
@@ -1,13 +1,52 @@
 // Name: Jai Vaidya
 // Roll: 2010990758
-// Set No: 
-// Q2: 
+// Set No: 03
+// Q2: Find a pair with the given sum in an array. Given an unsorted integer array, find a pair with the given sum in it.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
 
+    // input length
+    int length;
+    cout << "Enter length: ";
+    cin >> length;
+
+    // input target
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    // create arr
+    int arr[length];
+
+    // input array
+    cout << "Input array: ";
+    for (int i=0; i<length; i++) {
+        cin >> arr[i];
+    }
+
+    // creating a flag to check if pair is found or not
+    bool pairFound = false;
+
+    // checking pair sum
+    for (int i=0; i<length; i++) {
+        for (int j=i+1; j<length; j++) {
+            if (arr[i] + arr[j] == target) {
+                pairFound = true;
+                cout << "Pair found (" << arr[i] << ", " << arr[j] << ")" << endl;
+                break;
+            }
+        }
+    }
+
+    if (!pairFound) {
+        cout << "Pair not found" << endl;
+    }
+
+
+    // In this code, time complexity = O(N^2)
 
 
     return 0;
